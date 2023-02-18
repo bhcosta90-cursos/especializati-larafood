32805f975cfacc494c4e3d387936310ec0b85721
Bruno Costa <bruno.costa@dafiti.com.br>
2023-02-18 10:09:08 -0300

    finalizado a pagina inicial do site

--- app/Http/Controllers/Site/HomeController.php
+++ app/Http/Controllers/Site/HomeController.php
@@ -3,12 +3,15 @@
 namespace App\Http\Controllers\Site;
 
 use App\Http\Controllers\Controller;
+use App\Models\Plan;
 use Illuminate\Http\Request;
 
 class HomeController extends Controller
 {
     public function index()
     {
-        return view('site.home.index');
+        $plans = Plan::with('details')->orderBy('price')->get();
+
+        return view('site.home.index', compact('plans'));
     }
 }
