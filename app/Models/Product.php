a6eda2b892b083e1a791d9c4937d5bbe7108c678
Bruno Costa <bruno.costa@dafiti.com.br>
2023-02-19 09:00:24 -0300

    criando modelo de produto

---
+++ app/Models/Product.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Models;
+
+use App\Company\Traits\CompanyTrait;
+use Illuminate\Database\Eloquent\Concerns\HasUuids;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class Product extends Model
+{
+    use HasFactory, HasUuids, SoftDeletes, CompanyTrait;
+
+    protected $fillable = [
+        'title',
+        'flag',
+        'image',
+        'price',
+        'description',
+    ];
+
+    public function search(array $data)
+    {
+        return $this->where(function ($query) use ($data) {
+            if (!empty($filter = $data['search'] ?? null)) {
+                $query->where('name', 'like', "%{$filter}%")
+                ->orWhere('description', 'like', "%{$filter}%");
+            }
+        });
+    }
+}
